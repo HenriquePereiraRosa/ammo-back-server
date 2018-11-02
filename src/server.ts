2b4661f0dbeb965bc24889e2387c6ab008c52708
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-11-02 17:35:57 -0300

    Update src/server.ts
    Changed port and added ijmport for express.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 // src/server.ts
+
+const express = require('express');
+
+app.use(express.static(__dirname + '/dist'));
 // Import the app object
 import app from "./app";
 // Define a const for the port
-const PORT = 3000;
+const localPORT = 3000;
+
 // Start the server !
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+app.listen(process.env.PORT || localPORT, () => {
+    console.log('Express server listening on port ' + process.env.PORT);
 });
